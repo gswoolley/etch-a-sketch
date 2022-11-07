2939bed97873c2259b650f43f5114bf4ab50b978
Garett <g.woolley02@gmail.com>
2022-11-07 15:43:30 -0500

    Grid slider added

--- main.js
+++ main.js
@@ -2,18 +2,41 @@ const container = document.querySelector("#container");
 const clearBtn = document.querySelector("#clearBtn");
 let numSquares = 256;
 let gridSquares = document.getElementsByClassName("grid-element");
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+createSquares();
+
 
 //create grid of squares
-for (let i = 0; i < numSquares; i++) {
-  div = document.createElement("div");
-  div.className = "grid-element";
-  container.appendChild(div);
+function createSquares() {
+  reset();
+  for (let i = 0; i < numSquares; i++) {
+    div = document.createElement("div");
+    div.className = "grid-element";
+    container.appendChild(div);
+  }
 }
+
+function clear(){
+    for (let i = 0; i < gridSquares.length; i++) {
+        gridSquares[i].style.backgroundColor = "white";
+      }
+}
+
+//deletes all grid-elements
+function reset() {
+    document
+      .querySelectorAll(".grid-element")
+      .forEach((e) => e.parentNode.removeChild(e));
+  }
 
 //create 'etch a sketch' effect
 document.querySelectorAll("#container").forEach((item) => {
   item.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "pink";
+    container.style.backgroundColor = 'white';
+    event.target.style.backgroundColor = "black";
   });
 });
 
@@ -23,3 +46,18 @@ clearBtn.addEventListener("click", () => {
     gridSquares[i].style.backgroundColor = "white";
   }
 });
+
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+  numSquares = this.value ** 2;
+  numSquares = (this.value)*(this.value);
+  console.log(numSquares);
+  createSquares();
+  container.style.gridTemplateColumns = `repeat(${this.value}, 1fr)`
+  container.style.gridTemplateRows = `repeat(${this.value}, 1fr)`
+  container.style.backgroundColor = 'white';
+  
+};
+
