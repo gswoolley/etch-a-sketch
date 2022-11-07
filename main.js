4dcd91c680f42f6da8e99d332410282fec9c1fe6
Garett <g.woolley02@gmail.com>
2022-11-06 20:26:47 -0500

    added clear func

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-const container = document.querySelector('#container');
-const clearBtn = document.querySelector('#clearBtn')
+const container = document.querySelector("#container");
+const clearBtn = document.querySelector("#clearBtn");
 let numSquares = 256;
+let gridSquares = document.getElementsByClassName("grid-element");
 
-for(let i = 0; i < numSquares; i++){
-    div = document.createElement('div');
-    div.className= 'grid-element';
-    container.appendChild(div);
+//create grid of squares
+for (let i = 0; i < numSquares; i++) {
+  div = document.createElement("div");
+  div.className = "grid-element";
+  container.appendChild(div);
 }
 
-document.querySelectorAll('#container').forEach(item => {
-    item.addEventListener('mouseover', event => {
-        event.target.style.background = 'pink'
-    });
-  })
-
-  clearBtn.addEventListener('click', ()=>{
-        console.log('Cleared');
+//create 'etch a sketch' effect
+document.querySelectorAll("#container").forEach((item) => {
+  item.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "pink";
   });
+});
+
+//clear grid of squares of all colors
+clearBtn.addEventListener("click", () => {
+  for (let i = 0; i < gridSquares.length; i++) {
+    gridSquares[i].style.backgroundColor = "white";
+  }
+});
